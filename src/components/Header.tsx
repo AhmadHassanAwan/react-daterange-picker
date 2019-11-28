31e660d969c49067d24515616a469fc2cf60ea89
Ahmad Hassan <ahmad.hassan@face44.be>
2019-11-28 14:32:41 +0500

    passing theme from app to datepicker

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,12 @@
 import {
-	WithStyles,
-	Grid,
-	createStyles,
-	withStyles,
-	IconButton,
-	Select,
-	MenuItem,
-	Typography
+  WithStyles,
+  Grid,
+  createStyles,
+  withStyles,
+  IconButton,
+  Select,
+  MenuItem,
+  Typography
 } from "@material-ui/core";
 import React from "react";
 import ChevronLeft from "@material-ui/icons/ChevronLeft";
@@ -16,114 +16,119 @@ import ArrowRight from "@material-ui/icons/ArrowRight";
 import { setMonth, getMonth, setYear, getYear } from "date-fns";
 
 interface HeaderProps extends WithStyles<typeof styles> {
-	date: Date;
-	setDate: (date: Date) => void;
-	nextDisabled: boolean;
-	prevDisabled: boolean;
-	nextDisabledYear: boolean;
-	prevDisabledYear: boolean;
-	marker: symbol;
-	onClickNext: () => void;
-	onClickPrevious: () => void;
-	onClickNextYear: () => void;
-	onClickPreviousYear: () => void;
+  date: Date;
+  setDate: (date: Date) => void;
+  nextDisabled: boolean;
+  prevDisabled: boolean;
+  nextDisabledYear: boolean;
+  prevDisabledYear: boolean;
+  marker: symbol;
+  onClickNext: () => void;
+  onClickPrevious: () => void;
+  onClickNextYear: () => void;
+  onClickPreviousYear: () => void;
 }
 
 const styles = createStyles({
-	root: {
-		color: "#111111",
-		fontSize: 15,
-		"& p": {
-			paddingTop: 7,
-			fontWeight: "bold"
-		}
-	},
-	iconContainer: {
-		padding: 5
-	},
-	icon: {
-		padding: "3px 10px 0",
-		"&:hover": {
-			background: "none"
-		}
-	}
+  root: {
+    color: "#111111",
+    fontSize: 15,
+    "& p": {
+      paddingTop: 7,
+      fontWeight: "bold"
+    }
+  },
+  iconContainer: {
+    padding: 5
+  },
+  icon: {
+    padding: "3px 10px 0",
+    "&:hover": {
+      background: "none"
+    }
+  }
 });
 
 const MONTHS = [
-	"Jan",
-	"Feb",
-	"Mar",
-	"Apr",
-	"May",
-	"June",
-	"July",
-	"Aug",
-	"Sept",
-	"Oct",
-	"Nov",
-	"Dec"
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "June",
+  "July",
+  "Aug",
+  "Sept",
+  "Oct",
+  "Nov",
+  "Dec"
 ];
 
 const generateYears = (relativeTo: Date, count: number) => {
-	const half = Math.floor(count / 2);
-	return Array(count)
-		.fill(0)
-		.map((y, i) => relativeTo.getFullYear() - half + i); // TODO: make part of the state
+  const half = Math.floor(count / 2);
+  return Array(count)
+    .fill(0)
+    .map((y, i) => relativeTo.getFullYear() - half + i); // TODO: make part of the state
 };
 
 const Header: React.FunctionComponent<HeaderProps> = ({
-	date,
-	classes,
-	setDate,
-	marker,
-	nextDisabled,
-	prevDisabled,
-	nextDisabledYear,
-	prevDisabledYear,
-	onClickNext,
-	onClickPrevious,
-	onClickNextYear,
-	onClickPreviousYear
+  date,
+  classes,
+  setDate,
+  marker,
+  nextDisabled,
+  prevDisabled,
+  nextDisabledYear,
+  prevDisabledYear,
+  onClickNext,
+  onClickPrevious,
+  onClickNextYear,
+  onClickPreviousYear
 }) => {
-	console.log(marker);
-	const handleMonthChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
-		setDate(setMonth(date, parseInt(event.target.value)));
-	};
+  const handleMonthChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setDate(setMonth(date, parseInt(event.target.value)));
+  };
 
-	const handleYearChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
-		setDate(setYear(date, parseInt(event.target.value)));
-	};
+  const handleYearChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setDate(setYear(date, parseInt(event.target.value)));
+  };
 
-	return (
-		<Grid container justify="space-between" alignItems="center" className={classes.root}>
-			<Grid item className={classes.iconContainer}>
-				{/* {marker.toString() == "Symbol(firstMonth)" &&  */}
-				<React.Fragment>
-					<IconButton
-						className={classes.icon}
-						disabled={prevDisabled}
-						onClick={onClickPreviousYear}>
-						<ArrowLeft color={prevDisabledYear ? "disabled" : "action"} />
-					</IconButton>
-					<IconButton
-						className={classes.icon}
-						disabled={prevDisabled}
-						onClick={onClickPrevious}>
-						<ChevronLeft color={prevDisabled ? "disabled" : "action"} />
-					</IconButton>
-				</React.Fragment>
-				{/* // } */}
-			</Grid>
-			<Grid item>
-				<Typography>
-				{MONTHS[getMonth(date)]} {getYear(date)}
-
-				</Typography>
-				{/* {getMonth(date)}
+  return (
+    <Grid
+      container
+      justify="space-between"
+      alignItems="center"
+      className={classes.root}
+    >
+      <Grid item className={classes.iconContainer}>
+        {/* {marker.toString() == "Symbol(firstMonth)" &&  */}
+        <React.Fragment>
+          <IconButton
+            className={classes.icon}
+            disabled={prevDisabled}
+            onClick={onClickPreviousYear}
+          >
+            <ArrowLeft color={prevDisabledYear ? "disabled" : "action"} />
+          </IconButton>
+          <IconButton
+            className={classes.icon}
+            disabled={prevDisabled}
+            onClick={onClickPrevious}
+          >
+            <ChevronLeft color={prevDisabled ? "disabled" : "action"} />
+          </IconButton>
+        </React.Fragment>
+        {/* // } */}
+      </Grid>
+      <Grid item>
+        <Typography>
+          {MONTHS[getMonth(date)]} {getYear(date)}
+        </Typography>
+        {/* {getMonth(date)}
 				{MONTHS.filter((month, idx) => {
 					return (idx == date)
 				})} */}
-				{/* <Select
+        {/* <Select
 					value={getMonth(date)}
 					onChange={handleMonthChange}
 					MenuProps={{ disablePortal: true }}>
@@ -133,25 +138,30 @@ const Header: React.FunctionComponent<HeaderProps> = ({
 						</MenuItem>
 					))}
 				</Select> */}
-			</Grid>
+      </Grid>
 
-			
-			
-				<Grid item className={classes.iconContainer}>
-					{/* {marker.toString() == "Symbol(secondMonth)" && */}
-						<React.Fragment>
-							<IconButton className={classes.icon} disabled={nextDisabled} onClick={onClickNext}>
-								<ChevronRight color={nextDisabled ? "disabled" : "action"} />
-							</IconButton>
-							<IconButton className={classes.icon} disabled={nextDisabled} onClick={onClickNextYear}>
-								<ArrowRight color={nextDisabledYear ? "disabled" : "action"} />
-							</IconButton>
-						</React.Fragment>
-					{/* } */}
-				</Grid>
-			
-		</Grid>
-	);
+      <Grid item className={classes.iconContainer}>
+        {/* {marker.toString() == "Symbol(secondMonth)" && */}
+        <React.Fragment>
+          <IconButton
+            className={classes.icon}
+            disabled={nextDisabled}
+            onClick={onClickNext}
+          >
+            <ChevronRight color={nextDisabled ? "disabled" : "action"} />
+          </IconButton>
+          <IconButton
+            className={classes.icon}
+            disabled={nextDisabled}
+            onClick={onClickNextYear}
+          >
+            <ArrowRight color={nextDisabledYear ? "disabled" : "action"} />
+          </IconButton>
+        </React.Fragment>
+        {/* } */}
+      </Grid>
+    </Grid>
+  );
 };
 
 export default withStyles(styles)(Header);
--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,22 @@
 import * as React from "react";
-import { createStyles, WithStyles, withStyles, Theme } from "@material-ui/core";
 import {
-	addMonths,
-	isSameDay,
-	isWithinRange,
-	isAfter,
-	isBefore,
-	isSameMonth,
-	addYears,
-	max,
-	min,
-	differenceInCalendarDays
+  createStyles,
+  WithStyles,
+  withStyles,
+  Theme,
+  MuiThemeProvider
+} from "@material-ui/core";
+import {
+  addMonths,
+  isSameDay,
+  isWithinRange,
+  isAfter,
+  isBefore,
+  isSameMonth,
+  addYears,
+  max,
+  min,
+  differenceInCalendarDays
 } from "date-fns";
 import { DateRange, NavigationAction, DefinedRange } from "./types";
 import Menu from "./components/Menu";
@@ -20,170 +26,191 @@ import { parseOptionalDate } from "./utils";
 type Marker = symbol;
 
 export const MARKERS: { [key: string]: Marker } = {
-	FIRST_MONTH: Symbol("firstMonth"),
-	SECOND_MONTH: Symbol("secondMonth")
+  FIRST_MONTH: Symbol("firstMonth"),
+  SECOND_MONTH: Symbol("secondMonth")
 };
 
 const getValidatedMonths = (range: DateRange, minDate: Date, maxDate: Date) => {
-	let { startDate, endDate } = range;
-	if (startDate && endDate) {
-		const newStart = max(startDate, minDate);
-		const newEnd = min(endDate, maxDate);
+  let { startDate, endDate } = range;
+  if (startDate && endDate) {
+    const newStart = max(startDate, minDate);
+    const newEnd = min(endDate, maxDate);
 
-		return [newStart, isSameMonth(newStart, newEnd) ? addMonths(newStart, 1) : newEnd];
-	} else {
-		return [startDate, endDate];
-	}
+    return [
+      newStart,
+      isSameMonth(newStart, newEnd) ? addMonths(newStart, 1) : newEnd
+    ];
+  } else {
+    return [startDate, endDate];
+  }
 };
 
 const styles = (theme: Theme) =>
-	createStyles({
-		header: {
-			padding: "20px 70px"
-		},
-		headerItem: {
-			flex: 1
-		},
-		divider: {
-			borderLeft: `1px solid ${theme.palette.action.hover}`,
-			marginBottom: 20
-		}
-	});
+  createStyles({
+    header: {
+      padding: "20px 70px"
+    },
+    headerItem: {
+      flex: 1
+    },
+    divider: {
+      borderLeft: `1px solid ${theme.palette.action.hover}`,
+      marginBottom: 20
+    }
+  });
 
 interface DateRangePickerProps extends WithStyles<typeof styles> {
-	open: boolean;
-	initialDateRange?: DateRange;
-	definedRanges?: DefinedRange[];
-	minDate?: Date | string;
-	maxDate?: Date | string;
-	onChange: (dateRange: DateRange) => void;
+  open: boolean;
+  initialDateRange?: DateRange;
+  definedRanges?: DefinedRange[];
+  minDate?: Date | string;
+  theme: Theme;
+  maxDate?: Date | string;
+  onChange: (dateRange: DateRange) => void;
 }
 
 const DateRangePickerImpl: React.FunctionComponent<DateRangePickerProps> = props => {
-	const today = new Date();
+  const today = new Date();
 
-	const {
-		open,
-		onChange,
-		initialDateRange,
-		minDate,
-		maxDate,
-		definedRanges = defaultRanges
-	} = props;
+  const {
+    theme,
+    open,
+    onChange,
+    initialDateRange,
+    minDate,
+    maxDate,
+    definedRanges = defaultRanges
+  } = props;
 
-	const minDateValid = parseOptionalDate(minDate, addYears(today, -10));
-	const maxDateValid = parseOptionalDate(maxDate, addYears(today, 10));
-	const [intialFirstMonth, initialSecondMonth] = getValidatedMonths(
-		initialDateRange || {},
-		minDateValid,
-		maxDateValid
-	);
+  const minDateValid = parseOptionalDate(minDate, addYears(today, -10));
+  const maxDateValid = parseOptionalDate(maxDate, addYears(today, 10));
+  const [intialFirstMonth, initialSecondMonth] = getValidatedMonths(
+    initialDateRange || {},
+    minDateValid,
+    maxDateValid
+  );
 
-	// console.log("rendering DateRangePicker");
-	const [dateRange, setDateRange] = React.useState<DateRange>({ ...initialDateRange });
-	const [hoverDay, setHoverDay] = React.useState<Date>();
-	const [firstMonth, setFirstMonth] = React.useState<Date>(intialFirstMonth || today);
-	const [secondMonth, setSecondMonth] = React.useState<Date>(
-		initialSecondMonth || addMonths(firstMonth, 1)
-	);
+  // console.log("rendering DateRangePicker");
+  const [dateRange, setDateRange] = React.useState<DateRange>({
+    ...initialDateRange
+  });
+  const [hoverDay, setHoverDay] = React.useState<Date>();
+  const [firstMonth, setFirstMonth] = React.useState<Date>(
+    intialFirstMonth || today
+  );
+  const [secondMonth, setSecondMonth] = React.useState<Date>(
+    initialSecondMonth || addMonths(firstMonth, 1)
+  );
 
-	const { startDate, endDate } = dateRange;
+  const { startDate, endDate } = dateRange;
 
-	// handlers
-	const setFirstMonthValidated = (date: Date) => {
-		if (isBefore(date, secondMonth)) {
-			setFirstMonth(date);
-		}
-	};
+  // handlers
+  const setFirstMonthValidated = (date: Date) => {
+    if (isBefore(date, secondMonth)) {
+      setFirstMonth(date);
+    }
+  };
 
-	const setSecondMonthValidated = (date: Date) => {
-		if (isAfter(date, firstMonth)) {
-			setSecondMonth(date);
-		}
-	};
+  const setSecondMonthValidated = (date: Date) => {
+    if (isAfter(date, firstMonth)) {
+      setSecondMonth(date);
+    }
+  };
 
-	const setDateRangeValidated = (range: DateRange) => {
-		let { startDate: newStart, endDate: newEnd } = range;
-		if (newStart && newEnd) {
-			range.startDate = newStart = max(newStart, minDateValid);
-			range.endDate = newEnd = min(newEnd, maxDateValid);
-			setDateRange(range);
-			onChange(range);
-			setFirstMonth(newStart);
-			setSecondMonth(isSameMonth(newStart, newEnd) ? addMonths(newStart, 1) : newEnd);
-		}
-	};
+  const setDateRangeValidated = (range: DateRange) => {
+    let { startDate: newStart, endDate: newEnd } = range;
+    if (newStart && newEnd) {
+      range.startDate = newStart = max(newStart, minDateValid);
+      range.endDate = newEnd = min(newEnd, maxDateValid);
+      setDateRange(range);
+      onChange(range);
+      setFirstMonth(newStart);
+      setSecondMonth(
+        isSameMonth(newStart, newEnd) ? addMonths(newStart, 1) : newEnd
+      );
+    }
+  };
 
-	const onDayClick = (day: Date) => {
-		if (startDate && !endDate && !isBefore(day, startDate) && !isInvalidRange(day, startDate)) {
-			const newRange = { startDate, endDate: day };
-			onChange(newRange);
-			setDateRange(newRange);
-		} else {
-			setDateRange({ startDate: day, endDate: undefined });
-		}
-		setHoverDay(day);
-	};
+  const onDayClick = (day: Date) => {
+    if (
+      startDate &&
+      !endDate &&
+      !isBefore(day, startDate) &&
+      !isInvalidRange(day, startDate)
+    ) {
+      const newRange = { startDate, endDate: day };
+      onChange(newRange);
+      setDateRange(newRange);
+    } else {
+      setDateRange({ startDate: day, endDate: undefined });
+    }
+    setHoverDay(day);
+  };
 
-	const onMonthNavigate = (marker: Marker, action: NavigationAction) => {
-		if (marker == MARKERS.FIRST_MONTH) {
-			const firstNew = addMonths(firstMonth, action);
-			if (isBefore(firstNew, secondMonth)) setFirstMonth(firstNew);
-		} else {
-			const secondNew = addMonths(secondMonth, action);
-			if (isBefore(firstMonth, secondNew)) setSecondMonth(secondNew);
-		}
-	};
+  const onMonthNavigate = (marker: Marker, action: NavigationAction) => {
+    if (marker == MARKERS.FIRST_MONTH) {
+      const firstNew = addMonths(firstMonth, action);
+      if (isBefore(firstNew, secondMonth)) setFirstMonth(firstNew);
+    } else {
+      const secondNew = addMonths(secondMonth, action);
+      if (isBefore(firstMonth, secondNew)) setSecondMonth(secondNew);
+    }
+  };
 
-	const onDayHover = (date: Date) => {
-		if (startDate && !endDate) {
-			if (!hoverDay || !isSameDay(date, hoverDay)) {
-				setHoverDay(date);
-			}
-		}
-	};
-	
+  const onDayHover = (date: Date) => {
+    if (startDate && !endDate) {
+      if (!hoverDay || !isSameDay(date, hoverDay)) {
+        setHoverDay(date);
+      }
+    }
+  };
 
-	// helpers
-	const isInvalidRange = (endDate: any, startDate: any) => {
-		// out of 30 to 90 days range
-		return (differenceInCalendarDays(endDate, startDate) <30 || differenceInCalendarDays(endDate, startDate) > 90)
-	}
+  // helpers
+  const isInvalidRange = (endDate: any, startDate: any) => {
+    // out of 30 to 90 days range
+    return (
+      differenceInCalendarDays(endDate, startDate) < 30 ||
+      differenceInCalendarDays(endDate, startDate) > 90
+    );
+  };
 
-	const inHoverRange = (day: Date) => {
-		return (startDate &&
-			!endDate &&
-			hoverDay &&
-			isAfter(hoverDay, startDate) &&
-			isWithinRange(day, startDate, hoverDay)) as boolean;
-	};
+  const inHoverRange = (day: Date) => {
+    return (startDate &&
+      !endDate &&
+      hoverDay &&
+      isAfter(hoverDay, startDate) &&
+      isWithinRange(day, startDate, hoverDay)) as boolean;
+  };
 
-	const helpers = {
-		inHoverRange, isInvalidRange
-	};
+  const helpers = {
+    inHoverRange,
+    isInvalidRange
+  };
 
-	const handlers = {
-		onDayClick,
-		onDayHover,
-		onMonthNavigate
-	};
+  const handlers = {
+    onDayClick,
+    onDayHover,
+    onMonthNavigate
+  };
 
-	return open ? (
-		<Menu
-		hoverDay={hoverDay}
-			dateRange={dateRange}
-			minDate={minDateValid}
-			maxDate={maxDateValid}
-			ranges={definedRanges}
-			firstMonth={firstMonth}
-			secondMonth={secondMonth}
-			setFirstMonth={setFirstMonthValidated}
-			setSecondMonth={setSecondMonthValidated}
-			setDateRange={setDateRangeValidated}
-			helpers={helpers}
-			handlers={handlers}
-		/>
-	) : null;
+  return open ? (
+    <MuiThemeProvider theme={theme}>
+      <Menu
+        hoverDay={hoverDay}
+        dateRange={dateRange}
+        minDate={minDateValid}
+        maxDate={maxDateValid}
+        ranges={definedRanges}
+        firstMonth={firstMonth}
+        secondMonth={secondMonth}
+        setFirstMonth={setFirstMonthValidated}
+        setSecondMonth={setSecondMonthValidated}
+        setDateRange={setDateRangeValidated}
+        helpers={helpers}
+        handlers={handlers}
+      />
+    </MuiThemeProvider>
+  ) : null;
 };
 
 export { DateRange, DefinedRange } from "./types";
